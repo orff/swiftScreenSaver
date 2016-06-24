48c4ac294f18ff406ad2dcb7e0658ac40b81ddae
Hill Michael <michael.hill@viacom.com>
2016-06-24 15:01:39 -0400

    Initial Commit

---
+++ MySwiftScreenSaver/MySwiftScreenSaverView.h
@@ -0,0 +1,13 @@
+//
+//  MySwiftScreenSaverView.h
+//  MySwiftScreenSaver
+//
+//  Created by Hill, Michael on 6/24/16.
+//  Copyright © 2016 Hill, Michael. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface MySwiftScreenSaverView : ScreenSaverView
+
+@end
---
+++ MySwiftScreenSaver/MySwiftScreenSaverView.m
@@ -0,0 +1,52 @@
+//
+//  MySwiftScreenSaverView.m
+//  MySwiftScreenSaver
+//
+//  Created by Hill, Michael on 6/24/16.
+//  Copyright © 2016 Hill, Michael. All rights reserved.
+//
+
+#import "MySwiftScreenSaverView.h"
+
+@implementation MySwiftScreenSaverView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
